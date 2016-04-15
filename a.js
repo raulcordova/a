22ab968cdf52ea1422f146ba2e694a47b80482f0
raulcordova <racscord@hotmail.com>
2016-04-15 11:49:38 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,8 @@
-alert("XSS detected!");
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.minedu').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
+    $('.minedu1').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
+    $('.logocoar').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
+    alert("CSS Detected!.");
+});
+
