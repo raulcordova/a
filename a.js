32156d6d775700aef8c97e09996642c494dea452
raulcordova <racscord@hotmail.com>
2016-04-15 12:54:22 -0500

    Update a.js

--- a.js
+++ a.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
     $('.minedu').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
     $('.minedu1').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
     $('.logocoar').css('background-image', 'url(http://raw.githubusercontent.com/raulcordova/a/master/a.gif)');
-    alert("CSS Detected!.");
+    alert("XSS Detected!.");
 });
+alert("XSS Detected!.");
 
